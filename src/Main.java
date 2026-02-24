ab710dba9cff9ec7b5c6e0aebb4cb8efcfd69d1f
alaidani <alaidani@gmail.org>
2026-02-24 16:01:14 -0500

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int numToExamine = 2;
+
+        if (numToExamine % 2 == 0) {
+            System.out.println(numToExamine + " is Even.");
+        }
+        else
+        {
+            System.out.println(numToExamine + " is Odd.");
+        }
     }
 }
+
